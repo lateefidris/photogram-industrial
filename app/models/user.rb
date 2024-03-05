34cd343e76ede5f483c1c9b1c11d9a248ecc2dd7
Lateef Idris <lateef.idris17@gmail.com>
2024-03-05 14:00:52 -0600

    Added enum status to follow-requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,8 +33,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: "author_id"
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :recieved_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
-  has_many :accepted_sent_follow_requests, -> {where(status: "accepted")}, foreign_key: sender_id, class_name: "FollowRequest"
-  has_many :accepted_recieved_follow_requests, -> {where(status: "accepted")}, foreign_key: recipient_id, class_name: "FollowRequest"
+  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: sender_id, class_name: "FollowRequest"
+  has_many :accepted_recieved_follow_requests, -> { accepted }, foreign_key: recipient_id, class_name: "FollowRequest"
   has_many :likes, foreign_key: :fan_id
   has_many :leaders, through: :accepted_follow_requests, source: :recipient
   has_many :followers, through: :accepted_follow_requests, source: :sender
