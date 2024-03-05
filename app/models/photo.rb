e0fefaaf4644079908ea5804ff4b940af77f49e0
Lateef Idris <lateef.idris17@gmail.com>
2024-03-05 12:13:30 -0600

    Added Associations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -20,6 +20,8 @@
 #  fk_rails_...  (owner_id => users.id)
 #
 class Photo < ApplicationRecord
-  belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: "User", counter_cache: true
   has_many :comments
+  has_many :likes
+  has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
 end
