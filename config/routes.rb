fa159503d954cff0f7a5b08d10de503a996384a4
Lateef Idris <lateef.idris17@gmail.com>
2024-03-07 13:36:16 -0600

    better sample data

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "photos#index"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
