bb270e9e640f08c27960e8f18df893ed67055b7b
Lateef Idris <lateef.idris17@gmail.com>
2024-03-08 16:56:53 -0600

    Added link to liked photos

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
 
-  get "/:username" => "users#show"
+  get "/:username/liked" => "users#liked", as: :liked
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#following", as: :following
+
+  get "/:username" => "users#show", as: :user
   
   # Defines the root path route ("/")
   # root "articles#index"
